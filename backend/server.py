8f53824a93e2693ef2e88631ef950aeaaf3ca9aa
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-04-07 01:31:15 +0200

    fix backend

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,13 @@ from flask import Flask, request
 from flask_cors import CORS
  
 app = Flask(__name__)
-CORS(app, resources={r"/data": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/data": {"origins": "*"}})
  
 @app.route('/data', methods=['POST'])
 def getData():
-  return {
-    "result": request.json['years'] * request.json['degrees']
-  }
+  data = request.json
+  result = data['years'] * data['degrees']
+  return {"result": result}
  
 if __name__ == '__main__':
   app.run(debug=False)
